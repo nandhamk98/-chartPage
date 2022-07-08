93941ce28742bd67bb2cb80374af233ee935bede
Nandhakumar <nandhakumarmk98@gmail.com>
2022-07-08 23:00:41 +0530

    Charts created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  DoughnutController,
+  ArcElement,
+} from "chart.js";
+// import { DoughnutChart } from "./DoughnutChart";
+// import { LineChart } from "./LineChart";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  LineElement,
+  Tooltip,
+  Legend,
+  PointElement,
+  DoughnutController,
+  ArcElement
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <LineChart />
+      <DoughnutChart /> */}
     </div>
   );
 }
